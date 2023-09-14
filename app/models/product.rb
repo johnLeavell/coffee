3e0c949abba7f3152d2c324c6835eaea909a7f55
johnLeavell <johnleavell@gmail.com>
2023-09-14 10:45:36 -0500

    added category resource

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,15 @@
+# == Schema Information
+#
+# Table name: products
+#
+#  id          :integer          not null, primary key
+#  description :string
+#  name        :string
+#  price       :float
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  category_id :integer
+#
 class Product < ApplicationRecord
+  belongs_to :category, :required => true, :class_name => "Category", :foreign_key => "category_id", :counter_cache => true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_09_14_153744) do
+ActiveRecord::Schema.define(version: 2023_09_14_154418) do
 
   create_table "employees", force: :cascade do |t|
     t.string "email"
@@ -19,6 +19,15 @@ ActiveRecord::Schema.define(version: 2023_09_14_153744) do
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.boolean "admin"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.float "price"
+    t.integer "category_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
